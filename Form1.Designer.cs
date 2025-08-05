a22d0f54f1445821a4a159b42289dac9ae6cd841
tianyihang <xarzhi@noreply.gitcode.com>
2025-08-05 17:35:31 +0800

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿using System.Windows.Forms;
+
+namespace WinLock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(15, 23);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(47, 16);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "关机";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(15, 45);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(47, 16);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "重启";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(15, 67);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(47, 16);
+            this.radioButton3.TabIndex = 3;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "注销";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(15, 89);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(47, 16);
+            this.radioButton4.TabIndex = 4;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "睡眠";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(15, 112);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(47, 16);
+            this.radioButton5.TabIndex = 5;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "锁定";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton5);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(78, 147);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "操作";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.numericUpDown3);
+            this.groupBox2.Controls.Add(this.numericUpDown2);
+            this.groupBox2.Controls.Add(this.numericUpDown1);
+            this.groupBox2.Controls.Add(this.dateTimePicker1);
+            this.groupBox2.Controls.Add(this.radioButton7);
+            this.groupBox2.Controls.Add(this.radioButton6);
+            this.groupBox2.Location = new System.Drawing.Point(108, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(260, 147);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "时间";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(226, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 12);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "秒";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(140, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 12);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "分";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 12);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "时";
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.numericUpDown3.Location = new System.Drawing.Point(175, 77);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(41, 21);
+            this.numericUpDown3.TabIndex = 6;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(90, 77);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(43, 21);
+            this.numericUpDown2.TabIndex = 5;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(8, 77);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(47, 21);
+            this.numericUpDown1.TabIndex = 4;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "";
+            this.dateTimePicker1.Location = new System.Drawing.Point(113, 19);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(130, 21);
+            this.dateTimePicker1.TabIndex = 3;
+            this.dateTimePicker1.Value = new System.DateTime(2025, 8, 5, 0, 0, 0, 0);
+            this.dateTimePicker1.Visible = false;
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(60, 21);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(47, 16);
+            this.radioButton7.TabIndex = 1;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "定时";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            this.radioButton7.CheckedChanged += new System.EventHandler(this.radioButton7_CheckedChanged);
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Checked = true;
+            this.radioButton6.Location = new System.Drawing.Point(7, 21);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(47, 16);
+            this.radioButton6.TabIndex = 0;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "延时";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            this.radioButton6.CheckedChanged += new System.EventHandler(this.radioButton6_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(258, 180);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 39);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "开始任务";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Location = new System.Drawing.Point(13, 180);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(228, 39);
+            this.panel1.TabIndex = 9;
+            this.panel1.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 12);
+            this.label4.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(379, 234);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "WinLock";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.Button button1;
+        private DateTimePicker dateTimePicker1;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private NumericUpDown numericUpDown3;
+        private NumericUpDown numericUpDown2;
+        private NumericUpDown numericUpDown1;
+        private Panel panel1;
+        private Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static WinLock.Form1;
+
+namespace WinLock
+{
+    public partial class Form1 : Form
+    {
+        public bool isStart=false;
+        public int opt=1;
+        public int timeOpt=1;
+        public long timestampSeconds = 0;
+        public long leftTime = 0;
+
+        Timer timer1 = null;
+        public struct Date
+        {
+            public int year;
+            public int month;
+            public int day;
+            public int hour;
+            public int minute;
+            public int second;
+        }
+        Date date;
+
+        enum Option
+        {
+            关机=1, 重启, 注销, 睡眠, 锁定
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        public void SetEnable(bool enable)
+        {
+            radioButton1.Enabled = enable;
+            radioButton2.Enabled = enable;
+            radioButton3.Enabled = enable;
+            radioButton4.Enabled = enable;
+            radioButton5.Enabled = enable;
+            radioButton6.Enabled = enable;
+            radioButton7.Enabled = enable;
+            dateTimePicker1.Enabled = enable;
+            numericUpDown1.Enabled = enable;
+            numericUpDown2.Enabled = enable;
+            numericUpDown3.Enabled = enable;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+         
+            if (isStart)
+            {
+                panel1.Visible = false;
+                SetEnable(true);
+                timer1.Stop();           // 先停止
+                timer1.Dispose();        // 释放资源
+                timer1 = null;           // 可选：置空引用
+            }
+            else
+            {
+                panel1.Visible = true;
+               
+                if (timeOpt == 1)
+                {
+                    date.hour = (int)numericUpDown1.Value;
+                    date.minute = (int)numericUpDown2.Value;
+                    date.second = (int)numericUpDown3.Value;
+
+                    string hourStr = date.hour > 9 ? date.hour.ToString() : "0" + date.hour.ToString();
+                    string minuteStr = date.minute > 9 ? date.minute.ToString() : "0" + date.minute.ToString();
+                    string secondStr = date.second > 9 ? date.second.ToString() : "0" + date.second.ToString();
+
+                    leftTime = date.hour * 3600 + date.minute * 60 + date.second;
+
+                    label4.Text = "距离" + (Option)opt + "还有" + hourStr + ":" + minuteStr + ":" + secondStr;
+
+                }
+                else
+                {
+                    date.year = dateTimePicker1.Value.Year;
+                    date.month = dateTimePicker1.Value.Month;
+                    date.day = dateTimePicker1.Value.Day;
+                    date.hour = (int)numericUpDown1.Value;
+                    date.minute = (int)numericUpDown2.Value;
+                    date.second = (int)numericUpDown3.Value;
+
+                    string yearStr = date.year.ToString();
+                    string monthStr = date.month.ToString();
+                    string dayStr = date.day.ToString();
+                    string hourStr = date.hour> 9? date.hour.ToString():"0"+ date.hour.ToString();
+                    string minuteStr = date.minute > 9 ? date.minute.ToString() : "0" + date.minute.ToString();
+                    string secondStr = date.second > 9 ? date.second.ToString() : "0" + date.second.ToString();
+
+                    string timeStr= yearStr + "-" + monthStr + "-" + dayStr + " " + hourStr + ":" + minuteStr + ":" + secondStr;
+                    string format = "yyyy-M-d HH:mm:ss";
+                    DateTime dateTime = DateTime.ParseExact(
+                        timeStr,
+                        format,
+                        CultureInfo.InvariantCulture
+                    );
+                    DateTime utcDateTime = dateTime.ToUniversalTime();
+
+                    timestampSeconds = new DateTimeOffset(utcDateTime).ToUnixTimeSeconds();  // 设置时间的时间戳
+
+                  
+                    DateTime utcNow = DateTime.Now;
+                    long nowTimestamp = new DateTimeOffset(utcNow).ToUnixTimeSeconds();
+
+                    long lastTimestamp = timestampSeconds - nowTimestamp;
+                    if( lastTimestamp < 0)
+                    {
+                        MessageBox.Show("过去已是过去");
+                        return;
+                    }
+
+                    // 转换为 TimeSpan
+                    TimeSpan remainingTime = TimeSpan.FromSeconds(lastTimestamp);
+
+                    // 提取天、时、分、秒
+                    string daysStr = remainingTime.Days.ToString();
+                    string hoursStr = remainingTime.Hours.ToString();
+                    string minutesStr = remainingTime.Minutes.ToString();
+                    string secondsStr = remainingTime.Seconds.ToString();
+
+
+                    string text = "距离"+ yearStr+"年"+ monthStr + "月"+ dayStr + "日" + 
+                        " " + hourStr+ ":" + minuteStr+":"+ secondStr+ (Option)opt+"\n" +"还有"+ daysStr + "天"+ hoursStr + "时"+ minutesStr + "分"+ secondsStr + "秒";
+                        
+                    label4.Text = text;
+                }
+                timer1 = new Timer();
+                timer1.Interval = 1000;
+                timer1.Enabled = true;
+                timer1.Tick += new EventHandler(timer1EventProcessor);//添加事件
+                SetEnable(false);
+
+            }
+            button1.Text = isStart ? "开始任务" : "停止任务";
+            isStart = !isStart;
+        }
+        public void timer1EventProcessor(object source, EventArgs e)
+        {
+            timestampSeconds -= 1;
+
+           
+            if (timeOpt == 1)
+            {
+                leftTime -= 1;
+                if (leftTime < 0)
+                {
+                    timer1.Stop();           // 先停止
+                    timer1.Dispose();        // 释放资源
+                    timer1 = null;           // 可选：置空引用
+                    return;
+                }
+                date.hour = (int)numericUpDown1.Value;
+                date.minute = (int)numericUpDown2.Value;
+                date.second = (int)numericUpDown3.Value;
+
+                // 转换为 TimeSpan
+                TimeSpan remainingTime = TimeSpan.FromSeconds(leftTime);
+
+                // 提取天、时、分、秒
+                string hoursStr = remainingTime.Hours>9 ? remainingTime.Hours.ToString():"0" + remainingTime.Hours.ToString();
+                string minutesStr = remainingTime.Minutes > 9 ? remainingTime.Minutes.ToString() : "0" + remainingTime.Minutes.ToString();
+                string secondsStr = remainingTime.Seconds > 9 ? remainingTime.Seconds.ToString() : "0" + remainingTime.Seconds.ToString();
+
+
+
+                label4.Text = "距离" + (Option)opt + "还有" + hoursStr + ":" + minutesStr + ":" + secondsStr;
+
+            }
+            else
+            {
+                date.year = dateTimePicker1.Value.Year;
+                date.month = dateTimePicker1.Value.Month;
+                date.day = dateTimePicker1.Value.Day;
+                date.hour = (int)numericUpDown1.Value;
+                date.minute = (int)numericUpDown2.Value;
+                date.second = (int)numericUpDown3.Value;
+
+                string yearStr = date.year.ToString();
+                string monthStr = date.month.ToString();
+                string dayStr = date.day.ToString();
+                string hourStr = date.hour > 9 ? date.hour.ToString() : "0" + date.hour.ToString();
+                string minuteStr = date.minute > 9 ? date.minute.ToString() : "0" + date.minute.ToString();
+                string secondStr = date.second > 9 ? date.second.ToString() : "0" + date.second.ToString();
+
+                string timeStr = yearStr + "-" + monthStr + "-" + dayStr + " " + hourStr + ":" + minuteStr + ":" + secondStr;
+                string format = "yyyy-M-d HH:mm:ss";
+                DateTime dateTime = DateTime.ParseExact(
+                    timeStr,
+                    format,
+                    CultureInfo.InvariantCulture
+                );
+                DateTime utcDateTime = dateTime.ToUniversalTime();
+
+                timestampSeconds = new DateTimeOffset(utcDateTime).ToUnixTimeSeconds();  // 设置时间的时间戳
+
+
+                DateTime utcNow = DateTime.Now;
+                long nowTimestamp = new DateTimeOffset(utcNow).ToUnixTimeSeconds();
+
+                long lastTimestamp = timestampSeconds - nowTimestamp;
+                if (lastTimestamp < 0)
+                {
+                    timer1.Stop();           // 先停止
+                    timer1.Dispose();        // 释放资源
+                    timer1 = null;           // 可选：置空引用
+                    return;
+                }
+
+                // 转换为 TimeSpan
+                TimeSpan remainingTime = TimeSpan.FromSeconds(lastTimestamp);
+
+                // 提取天、时、分、秒
+                string daysStr = remainingTime.Days.ToString();
+                string hoursStr = remainingTime.Hours.ToString();
+                string minutesStr = remainingTime.Minutes.ToString();
+                string secondsStr = remainingTime.Seconds.ToString();
+
+
+                string text = "距离" + yearStr + "年" + monthStr + "月" + dayStr + "日" +
+                    " " + hourStr + ":" + minuteStr + ":" + secondStr + (Option)opt+"\n" + "还有" + daysStr + "天" + hoursStr + "时" + minutesStr + "分" + secondsStr + "秒";
+
+                label4.Text = text;
+
+            }
+
+          
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (((RadioButton)sender).Checked)
+            {
+                opt = 1;
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (((RadioButton)sender).Checked)
+            {
+                opt = 2;
+            }
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (((RadioButton)sender).Checked)
+            {
+                opt = 3;
+            }
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (((RadioButton)sender).Checked)
+            {
+                opt = 4;
+            }
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+            if (((RadioButton)sender).Checked)
+            {
+                opt = 5;
+            }
+        }
+
+        private void radioButton6_CheckedChanged(object sender, EventArgs e)
+        {
+            if (((RadioButton)sender).Checked)
+            {
+                timeOpt = 1;
+                dateTimePicker1.Hide();
+            }
+        }
+        private void radioButton7_CheckedChanged(object sender, EventArgs e)
+        {
+            if (((RadioButton)sender).Checked)
+            {
+                timeOpt = 2;
+                dateTimePicker1.Show();
+                numericUpDown1.Maximum = 24;
+            }
+        }
+    }
+}
